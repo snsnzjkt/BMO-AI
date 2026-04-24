e0cd03ee4cd6551a333579c82c5c8b65d21542bf
snsnzjkt <edrineesguerra4@gmail.com>
2026-04-24 13:59:47 +0800

    fix(brain): reflect request origin on SSE endpoint to support file:// clients

--- services/brain/src/routes/state.js
+++ services/brain/src/routes/state.js
@@ -24,7 +24,7 @@ router.get('/stream', (req, res) => {
   res.setHeader('Content-Type', 'text/event-stream');
   res.setHeader('Cache-Control', 'no-cache');
   res.setHeader('Connection', 'keep-alive');
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', req.headers.origin || '*');
   res.flushHeaders();
 
   res.write(`data: ${JSON.stringify({ state: store.currentState })}\n\n`);
